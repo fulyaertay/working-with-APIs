f257d489135bae2eb7d76de74eb5939a92eeedf7
Fulya <fulya.ertay@gmail.com>
2023-03-02 16:35:58 +0300

    changed layout style

--- 1-BoredBot-app/index.js
+++ 1-BoredBot-app/index.js
@@ -1,15 +1,23 @@
 /**
 Challenge: 
 
-When the button is clicked, call out to the Bored API
-(URL: https://apis.scrimba.com/bored/api/activity)
-and replace the h4 with the activity from the API
-
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
 */
+
 document.getElementById("get-activity").addEventListener("click", function() {
     fetch("https://apis.scrimba.com/bored/api/activity")
       .then(response => response.json())
       .then(data => {
         document.getElementById("activity").textContent = data.activity
+        document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        document.body.classList.add("fun")
       })
   })
