610096e4e69047ddcf99349613b3bf94ac08558c
Fulya <fulya.ertay@gmail.com>
2023-03-03 20:31:18 +0300

    refactored css

--- 3-color-schema-generator/index.js
+++ 3-color-schema-generator/index.js
@@ -1,58 +1,60 @@
-const chooseColorFromPicker=document.querySelector(".choose-color")
-const getColorFromSelectBox=document.querySelector("#colors")
-const getColorBtn=document.querySelector("#get-color")
-let listColors= document.querySelector(".list-colors")
-let hexValuesContent=document.querySelector(".hex-values")
-let listItem=document.querySelector(".list-color")
-let renderColors=""
-let renderHexValues=""
+const chooseColorFromPicker = document.querySelector(".choose-color");
+const getColorFromSelectBox = document.querySelector("#colors");
+const getColorBtn = document.querySelector("#get-color");
+let listColors = document.querySelector(".list-colors");
+let hexValuesContent = document.querySelector(".hex-values");
+let listItem = document.querySelector(".list-color");
+const pickerVal = chooseColorFromPicker.value;
+const listVal = getColorFromSelectBox.value;
 
-function firstRender(){
-    fetch("https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome")
-    .then(res => res.json())
-    .then(data => {
-        for (let color of data.colors){
-            renderColors+=`
+function firstRender() {
+  let renderColors = "";
+  let renderHexValues = "";
+
+  fetch("https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome")
+    .then((res) => res.json())
+    .then((data) => {
+      for (let color of data.colors) {
+        renderColors += `
             <div class="list-color" style="background-color:${color.hex.value}; cursor:pointer"></div>
 
-            `
-            renderHexValues+=` <div class="color-hex">${color.hex.value}</div>`
-           
+            `;
+        renderHexValues += ` <div class="color-hex">${color.hex.value}</div>`;
+      }
 
-        }
-        listColors.innerHTML=renderColors
-        hexValuesContent.innerHTML=renderHexValues
-        
-       
-    })
-
+      listColors.innerHTML = renderColors;
+      hexValuesContent.innerHTML = renderHexValues;
+    });
 }
 
+function render() {
+  let renderColors = "";
+  let renderHexValues = "";
 
-function render(){
-    const pickerVal=chooseColorFromPicker.value
-    const listVal=getColorFromSelectBox.value
-    //console.log(listVal)
+  const pickerVal = chooseColorFromPicker.value;
+  const listVal = getColorFromSelectBox.value;
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${pickerVal.slice(1)}&mode=${listVal}`)
-    .then(res => res.json())
-    .then(data => {
-        for (let color of data.colors){
-            renderColors+=`
+  //console.log(listVal)
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${pickerVal.slice(
+      1
+    )}&mode=${listVal}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      for (let color of data.colors) {
+        renderColors += `
             <div class="list-color" style="background-color:${color.hex.value}; cursor:pointer"></div>
 
-            `
-            renderHexValues+=` <div class="color-hex">${color.hex.value}</div>`
-           
+            `;
+        renderHexValues += ` <div class="color-hex">${color.hex.value}</div>`;
+      }
 
-        }
-        listColors.innerHTML=renderColors
-        hexValuesContent.innerHTML=renderHexValues
-        
-       
-    })
-
+      listColors.innerHTML = renderColors;
+      hexValuesContent.innerHTML = renderHexValues;
+    });
 }
 
-firstRender()
-getColorBtn.addEventListener("click",render)
+firstRender();
+getColorBtn.addEventListener("click", render);
