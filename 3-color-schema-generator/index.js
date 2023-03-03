f6d78e7fe37991b8c705ba99692bff73042b2b5d
Fulya <fulya.ertay@gmail.com>
2023-03-03 19:13:36 +0300

    updated color picker

--- 3-color-schema-generator/index.js
+++ 3-color-schema-generator/index.js
@@ -9,19 +9,20 @@ function render(){
     const pickerVal=chooseColorFromPicker.value
     const listVal=getColorFromSelectBox.value
     //console.log(listVal)
-
+    let html=""
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${pickerVal.slice(1)}`)
     .then(res => res.json())
     .then(data => {
         for (let color of data.colors){
-            listColors.innerHTML+=`
+            html+=`
             <div class="list-color" style="background-color:${color.hex.value}">${color.hex.value}</div>
 
             `
            
 
         }
+        listColors.innerHTML=html
         
        
     })
