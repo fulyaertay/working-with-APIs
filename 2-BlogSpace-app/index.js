44d1d179ba5c3a1cde49bd9c0e4bc3e0d563ff02
Fulya <fulya.ertay@gmail.com>
2023-03-03 12:42:29 +0300

    get first 5 objects

--- 2-BlogSpace-app/index.js
+++ 2-BlogSpace-app/index.js
@@ -1,51 +1,21 @@
-let postsArray = []
-const titleInput = document.getElementById("post-title")
-const bodyInput = document.getElementById("post-body")
-const form = document.getElementById("new-post")
+/**
+ Challenge:
+ 
+ GET a list of blog posts from the JSON Placeholder API.
+ 
+ BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ Endpoint: /posts
+ 
+ Since there's so many posts, let's limit the array to just 5 items.
+ You can use the `.slice()` array method to just grab the first 5 objects
+ from the data array that comes back from the API
+ 
+ Log the 5 items to the console
+ */
 
-function renderPosts() {
-    let html = ""
-    for (let post of postsArray) {
-        html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />
-        `
-    }
-    document.getElementById("blog-list").innerHTML = html
-}
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        postsArray = data.slice(0, 5)
-        renderPosts()
-    })
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const postTitle = titleInput.value
-    const postBody = bodyInput.value
-    const data = {
-        title: postTitle,
-        body: postBody
-    }
-    
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            postsArray.unshift(post)
-            renderPosts()
-            titleInput.value = ""
-            bodyInput.value = ""
-            // form.reset()
-        })
-})
+ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+ .then(res => res.json())
+ .then(data => {
+     const postsArr = data.slice(0, 5)
+     console.log(postsArr)
+ })
