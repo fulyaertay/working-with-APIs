f74f40e5094497f9865c10840fc2540ff4d6d826
Fulya <fulya.ertay@gmail.com>
2023-03-03 19:20:22 +0300

    edited hex values

--- 3-color-schema-generator/index.js
+++ 3-color-schema-generator/index.js
@@ -9,20 +9,24 @@ function render(){
     const pickerVal=chooseColorFromPicker.value
     const listVal=getColorFromSelectBox.value
     //console.log(listVal)
-    let html=""
+    let renderColors=""
+    let renderHexValues=""
+
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${pickerVal.slice(1)}`)
     .then(res => res.json())
     .then(data => {
         for (let color of data.colors){
-            html+=`
+            renderColors+=`
             <div class="list-color" style="background-color:${color.hex.value}">${color.hex.value}</div>
 
             `
+            renderHexValues+=` <div class="color-hex">${color.hex.value}</div>`
            
 
         }
-        listColors.innerHTML=html
+        listColors.innerHTML=renderColors
+        hexValuesContent.innerHTML=renderHexValues
         
        
     })
