be407435d67e256bd9c3e67ce0baafa2d37bcb38
Fulya <fulya.ertay@gmail.com>
2023-03-03 13:02:59 +0300

    rendered posts on html

--- 2-BlogSpace-app/index.js
+++ 2-BlogSpace-app/index.js
@@ -1,21 +1,26 @@
 /**
  Challenge:
+
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
  
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
- 
- Log the 5 items to the console
+ Hints: 
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you 
+   can then put into the div with `innerHTML`
  */
 
- fetch("https://apis.scrimba.com/jsonplaceholder/posts")
- .then(res => res.json())
- .then(data => {
-     const postsArr = data.slice(0, 5)
-     console.log(postsArr)
- })
+   fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+   .then(res => res.json())
+   .then(data => {
+       const postsArr = data.slice(0, 5)
+       let html = ""
+       for (let post of postsArr) {
+           html += `
+               <h3>${post.title}</h3>
+               <p>${post.body}</p>
+               <hr />
+           `
+       }
+       document.getElementById("blog-list").innerHTML = html
+   })
