867c96b78e611cb31e69bcd4e94d0ace0d7e3470
Fulya <fulya.ertay@gmail.com>
2023-03-05 15:31:44 +0300

    added determineCardWinner

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -28,9 +28,50 @@ drawCardBtn.addEventListener("click", () => {
 /**
  * Challenge:
  * 
- * Place each of the cards we draw into its respective card-slot
- * Hint: consider using element.children in the DOM instead of
- * giving each card-slot its own unique ID
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
  * 
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/children
+ * In parts:
+ * 
+ * 1. Create a function that takes 2 card objects as parameters, 
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ * 
+ * "2", "3", "4", "5", "6", "7", "8", "9", 
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ * 
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ * 
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ * 
+ * Log which card wins (or "It's a tie!" 
+ * if they're the same) to the console
  */
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log("Card 1 wins!")
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+const card1Obj = {
+    value: "JACK"
+}
+const card2Obj = {
+    value: "QUEEN"
+}
+
+determineCardWinner(card1Obj, card2Obj)
