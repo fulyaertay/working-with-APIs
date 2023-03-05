080ab57848fdb66d3790403bb2a9e51528709ebd
Fulya <fulya.ertay@gmail.com>
2023-03-05 14:19:20 +0300

    added draw-cards

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -1,4 +1,4 @@
-let deckId=""
+let deckId
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -10,15 +10,25 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
 /**
  * Challenge
  * 
- * Background:
- * The Deck of Cards API expects us to provide the deck id 
- * of the deck we're playing with so it can remember which
- * cards we've already drawn, how many are remaining in the
- * deck, etc.
+ * Task: Using the saved deckId, draw 2 new cards from the deck
  * 
- * Task: save the deck_id from the returned data to a local
- * variable so we can use it later
+ * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+ * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+ * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+ * that you'll see in the deck of cards API docs.
+ * 
+ * 1. Create a new button that, when clicked, draws 2 cards from the deckId
+ * you have saved
+ *      Note: you'll need to get a new deck every time you refresh the page,
+ *      since you're only saving your deckId in a local variable right now
+ * 2. Log those 2 cards to the console
  */
