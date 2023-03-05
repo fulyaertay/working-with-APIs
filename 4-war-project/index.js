d7a2e91f2b8f6637e3f403c64fff9918ddc911ce
Fulya <fulya.ertay@gmail.com>
2023-03-05 12:19:40 +0300

    added calback

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -1,68 +1,21 @@
-let deckId
-let computerScore = 0
-let myScore = 0
-const cardsContainer = document.getElementById("cards")
-const newDeckBtn = document.getElementById("new-deck")
-const drawCardBtn = document.getElementById("draw-cards")
-const header = document.getElementById("header")
-const remainingText = document.getElementById("remaining")
-const computerScoreEl = document.getElementById("computer-score")
-const myScoreEl = document.getElementById("my-score")
-
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            deckId = data.deck_id
-            console.log(deckId)
-        })
+        .then(data => console.log(data))
 }
 
-newDeckBtn.addEventListener("click", handleClick)
+document.getElementById("new-deck").addEventListener("click", handleClick)
 
-drawCardBtn.addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class="card" />
-            `
-            cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class="card" />
-            `
-            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            header.textContent = winnerText
-            
-            if (data.remaining === 0) {
-                drawCardBtn.disabled = true
-                if (computerScore > myScore) {
-                    header.textContent = "The computer won the game!"
-                } else if (myScore > computerScore) {
-                    header.textContent = "You won the game!"
-                } else {
-                    header.textContent = "It's a tie game!"
-                }
-            }
-        })
-})
+/**
+ * Challenge: 
+ * 
+ * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
+ * 
+ * Part 2: Move the anonymous in-line function to its own, named function
+ */
 
-function determineCardWinner(card1, card2) {
-    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
-    "10", "JACK", "QUEEN", "KING", "ACE"]
-    const card1ValueIndex = valueOptions.indexOf(card1.value)
-    const card2ValueIndex = valueOptions.indexOf(card2.value)
-    
-    if (card1ValueIndex > card2ValueIndex) {
-        computerScore++
-        computerScoreEl.textContent = `Computer score: ${computerScore}`
-        return "Computer wins!"
-    } else if (card1ValueIndex < card2ValueIndex) {
-        myScore++
-        myScoreEl.textContent = `My score: ${myScore}`
-        return "You win!"
-    } else {
-        return "War!"
-    }
+function callback() {
+    console.log("I finally ran!")
 }
+
+setTimeout(callback, 2000)
