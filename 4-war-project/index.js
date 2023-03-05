b126b89ae54ac4e619b988ba5bae747506dc2e4e
Fulya <fulya.ertay@gmail.com>
2023-03-05 15:45:41 +0300

    added determineCardWinner

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -2,12 +2,14 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
@@ -23,6 +25,8 @@ drawCardBtn.addEventListener("click", () => {
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
         })
 })
 /**
@@ -31,23 +35,14 @@ drawCardBtn.addEventListener("click", () => {
  * Try to determine which of the 2 cards is the "winner" (has higher value)
  * Aces are the card with the highest "score"
  * 
- * In parts:
+ * Part 2:
+ * Instead of logging the winner to the console, 
+ * display an `h2` on the screen above the 2 cards 
+ * that declares who the winner is.
  * 
- * 1. Create a function that takes 2 card objects as parameters, 
- * `card1` and `card2`. These card objects have a property called
- * `value`, which can be any one of the following strings, in
- * order of rising "score":
- * 
- * "2", "3", "4", "5", "6", "7", "8", "9", 
- * "10", "JACK", "QUEEN", "KING", "ACE"
- * 
- * I.e. "2" is the lowest score and "ACE" is the highest.
- * 
- * The function should determine which of the 2 cards (`card1`
- * or `card2`) has the higher score, or if they have the same score.
- * 
- * Log which card wins (or "It's a tie!" 
- * if they're the same) to the console
+ * If card1 is the higher card, display "Computer wins!"
+ * If card2 is the higher card, display "You win!"
+ * If they're equal, display "War!"
  */
 
 function determineCardWinner(card1, card2) {
@@ -55,23 +50,22 @@ function determineCardWinner(card1, card2) {
     "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
-    console.log("card 1:", card1ValueIndex)
-    console.log("card 2:", card2ValueIndex)
     
     if (card1ValueIndex > card2ValueIndex) {
-        console.log("Card 1 wins!")
+        return "Card 1 wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        console.log("Card 2 wins!")
+        return "Card 2 wins!"
     } else {
-        console.log("It's a tie!")
+        return "War!"
     }
 }
 
-const card1Obj = {
-    value: "JACK"
-}
-const card2Obj = {
-    value: "QUEEN"
-}
+// Keeping this test case for reference, but should delete later
+// const card1Obj = {
+//     value: "JACK"
+// }
+// const card2Obj = {
+//     value: "QUEEN"
+// }
 
-determineCardWinner(card1Obj, card2Obj)
+// determineCardWinner(card1Obj, card2Obj)
