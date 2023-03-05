c70ea0bfde38fe01b217be15c96de90124201845
Fulya <fulya.ertay@gmail.com>
2023-03-05 13:36:13 +0300

    added voters

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -1,58 +1,25 @@
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
-
-document.getElementById("new-deck").addEventListener("click", handleClick)
-
-// function callback() {
-//     console.log("I finally ran!")
-// }
-
-// setTimeout(callback, 2000)
-
-// const people = [
-//     { name: "Jack", hasPet: true },
-//     { name: "Jill", hasPet: false },
-//     { name: "Alice", hasPet: true },
-//     { name: "Bob", hasPet: false },
-// ]
-
-// function gimmeThePets(number) {
-//     return person.hasPet
-// }
-
-// const peopleWithPets = people.filter(gimmeThePets)
-// console.log(peopleWithPets)
-
-const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
-]
-
-function filterArray(array, callback) {
-    const resultingArray = []
-    // Write your filtering logic here
-    for (let item of array) {
-        const shouldBeIncluded = callback(item)
-        if (shouldBeIncluded) {
-            resultingArray.push(item)
-        }
-    }
-    return resultingArray
-}
-
 /**
- * Challenge: Use your filter array method!
- * Given the above `people` array, return a new array with only people where `hasPet` is true
- * Note: Remember that your callback function will be given the individual item in the array for a parameter
+ * Challenge: method chaining!
+ * 
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ *    - I realize this might feel like busywork, but my intent will make sense soon
+ * 
+ * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
  */
 
-const peopleWithPets = filterArray(people, function(person) {
-    return person.hasPet
-})
+// document.getElementById("new-deck").addEventListener("click", function() {
+//     console.log("Clicked!")
+// })
 
-console.log(peopleWithPets)
+const voters = [
+    {name: "Joe", email: "joe@joe.com", voted: true},
+    {name: "Jane", email: "jane@jane.com", voted: true},
+    {name: "Bo", email: "bo@bo.com", voted: false},
+    {name: "Bane", email: "bane@bane.com", voted: false}
+]
+
+// Write your code below
+const finalResult = voters.filter(voter => voter.voted).map(voter => voter.email)
+console.log(finalResult)
+
+// Final result: ["joe@joe.com", "jane@jane.com"]
