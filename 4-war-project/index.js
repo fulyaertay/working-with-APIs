6479345e75f2fba29edfccb1cd68b5af9021e017
Fulya <fulya.ertay@gmail.com>
2023-03-05 13:42:14 +0300

    added promise

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -1,25 +1,16 @@
+// function handleClick() {
+//     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// }
+
+// document.getElementById("new-deck").addEventListener("click", handleClick)
+
+const promise = fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+console.log(promise.then(res => res.json()))
+
 /**
- * Challenge: method chaining!
- * 
- * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
- *    - I realize this might feel like busywork, but my intent will make sense soon
- * 
- * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
+ * Mini challenge: Figure out what `promise.then()` returns! 
+ * Save the result to a variable and log it to the console.
  */
-
-// document.getElementById("new-deck").addEventListener("click", function() {
-//     console.log("Clicked!")
-// })
-
-const voters = [
-    {name: "Joe", email: "joe@joe.com", voted: true},
-    {name: "Jane", email: "jane@jane.com", voted: true},
-    {name: "Bo", email: "bo@bo.com", voted: false},
-    {name: "Bane", email: "bane@bane.com", voted: false}
-]
-
-// Write your code below
-const finalResult = voters.filter(voter => voter.voted).map(voter => voter.email)
-console.log(finalResult)
-
-// Final result: ["joe@joe.com", "jane@jane.com"]
+    // .then(data => console.log(data))
