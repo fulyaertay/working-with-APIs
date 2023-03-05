60d236e92b4cf141eeca2788fcfd6036b0653a8d
Fulya <fulya.ertay@gmail.com>
2023-03-05 15:19:00 +0300

    refactored image slot

--- 4-war-project/index.js
+++ 4-war-project/index.js
@@ -1,23 +1,26 @@
 let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            document.getElementById("cards").innerHTML = `
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
         })
@@ -25,6 +28,9 @@ document.getElementById("draw-cards").addEventListener("click", () => {
 /**
  * Challenge:
  * 
- * Create a spot in the HTML for the cards to be placed in.
- * Typical playing cards have a 5:7 ratio (width-to-height).
+ * Place each of the cards we draw into its respective card-slot
+ * Hint: consider using element.children in the DOM instead of
+ * giving each card-slot its own unique ID
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/children
  */
