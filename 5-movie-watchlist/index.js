061eab6c957b3d2a65ac4fea1a024406b011aa86
Fulya <fulya.ertay@gmail.com>
2023-03-06 18:02:41 +0300

    added renderMovies

--- 5-movie-watchlist/index.js
+++ 5-movie-watchlist/index.js
@@ -1,19 +1,28 @@
-"https://www.omdbapi.com/?t=aa&apikey=60c3cf12"
+"https://www.omdbapi.com/?t=aa&apikey=60c3cf12";
 
-const submitBtn = document.querySelector(".submit-btn")
-const searchInput = document.querySelector(".search-input")
-let container = document.querySelector(".container")
+const submitBtn = document.querySelector(".submit-btn");
+const searchInput = document.querySelector(".search-input");
+let container = document.querySelector(".container");
 
-function renderMovies(){
-    if (searchInput.value==""){
-        container.innerHTML=`
+function renderMovies() {
+  if (searchInput.value == "") {
+    container.innerHTML = `
         <img src="./images/no-data-initial.png" class="no-data" />
-        `
-
-    }
+        `;
+  }
+  submitBtn.addEventListener("click", () => {
+    fetch(`https://www.omdbapi.com/?t=${searchInput.value}&apikey=60c3cf12`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.Title == undefined) {
+          container.innerHTML = `
+        <h2 class="no-movie">Movie not found!</h2>
+        `;
+        }
+        
+      });
+    searchInput.value = "";
+  });
 }
 
-
-
-
-renderMovies()
+renderMovies();
