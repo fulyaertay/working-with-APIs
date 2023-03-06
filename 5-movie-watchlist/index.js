e7d5fd2ab8546ec11c61759268c7d04bd5959c8d
Fulya <fulya.ertay@gmail.com>
2023-03-06 20:00:31 +0300

    fetched movies from OMDB API

--- 5-movie-watchlist/index.js
+++ 5-movie-watchlist/index.js
@@ -5,21 +5,98 @@ const searchInput = document.querySelector(".search-input");
 let container = document.querySelector(".container");
 
 function renderMovies() {
-  if (searchInput.value == "") {
-    container.innerHTML = `
-        <img src="./images/no-data-initial.png" class="no-data" />
-        `;
-  }
+  //   if (searchInput.value == "") {
+  //     container.innerHTML = `
+  //         <img src="./images/no-data-initial.png" class="no-data" />
+
+  //         `;
+  //   }
   submitBtn.addEventListener("click", () => {
     fetch(`https://www.omdbapi.com/?t=${searchInput.value}&apikey=60c3cf12`)
       .then((res) => res.json())
       .then((data) => {
-        if (data.Title == undefined) {
-          container.innerHTML = `
-        <h2 class="no-movie">Movie not found!</h2>
-        `;
-        }
+        // if (data.Title == undefined) {
+        //   container.innerHTML = `
+        // <h2 class="no-movie">Movie not found!</h2>
+        // `;
+        // }
+
+        container.innerHTML = `
+
+        <div class="movie-container">
+            <div class="movie-card">
+                <div class="poster-image">
+                    <img src="${data.Poster}" class="movie-image">
+                </div>
+           
+                <div class="movie-content">
+
+                    <div class="movie-info">
+                        <h3>${data.Title}</h3>
+                        <img src="./images/star-icon.png" class="star-icon">
+                        <h5>${data.imdbRating}</h5>
+                        
+                    </div>
+
+                    <div class="movie-info">
+                        <h5>${data.Runtime}</h5>
+                        <h5 class="genre">${data.Genre}</h5>
+                        <img src="./images/my-watclish-icon.png" class="star-icon">
+                        <h5 class="add-watchlist">Watchlist</h5>
+                    
+                    </div>
+
+
+                    <div class="movie-info">
+                        <p class="movie-text">${data.Plot}</p>
+                    </div>
         
+                </div>
+                
+        
+            </div>
+
+
+
+            <div class="movie-card">
+                <div class="poster-image">
+                    <img src="${data.Poster}" class="movie-image">
+                </div>
+           
+                <div class="movie-content">
+
+                    <div class="movie-info">
+                        <h3>${data.Title}</h3>
+                        <img src="./images/star-icon.png" class="star-icon">
+                        <h5>${data.imdbRating}</h5>
+                        
+                    </div>
+
+                    <div class="movie-info">
+                        <h5>${data.Runtime}</h5>
+                        <h5 class="genre">${data.Genre}</h5>
+                        <img src="./images/my-watclish-icon.png" class="star-icon">
+                        <h5 class="add-watchlist">Watchlist</h5>
+                    
+                    </div>
+
+
+                    <div class="movie-info">
+                        <p class="movie-text">${data.Plot}</p>
+                    </div>
+        
+                </div>
+                
+        
+            </div>
+
+        </div>
+
+        
+        
+        
+        
+        `;
       });
     searchInput.value = "";
   });
