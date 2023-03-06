bb6f300dc4f9765c8ef6b061cdb6d6da5e9b7e29
Fulya <fulya.ertay@gmail.com>
2023-03-06 20:54:59 +0300

    added movies

--- 5-movie-watchlist/index.js
+++ 5-movie-watchlist/index.js
@@ -12,7 +12,7 @@ function renderMovies() {
           `;
   }
   submitBtn.addEventListener("click", () => {
-    fetch(`https://www.omdbapi.com/?s=${searchInput.value}&apikey=60c3cf12`)
+    fetch(`https://www.omdbapi.com/?t=${searchInput.value}&apikey=60c3cf12&type=movie&r=json`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -22,35 +22,35 @@ function renderMovies() {
             <h2 class="no-movie">Movie not found!</h2>
          `;
         } else {
-          for (movie of data.Search) {
+         
             container.innerHTML += `
 
 
                 <div class="movie-card">
                     <div class="poster-image">
-                        <img src="${movie.Poster}" class="movie-image">
+                        <img src="${data.Poster}" class="movie-image">
                     </div>
             
                     <div class="movie-content">
 
                         <div class="movie-info">
-                            <h3>${movie.Title}</h3>
+                            <h3>${data.Title}</h3>
                             <img src="./images/star-icon.png" class="star-icon">
-                            <h5>${movie.imdbRating}</h5>
+                            <h5>${data.imdbRating}</h5>
                             
                         </div>
 
                         <div class="movie-info">
-                            <h5>${movie.Runtime}</h5>
-                            <h5 class="genre">${movie.Genre}</h5>
-                            <img src="./images/my-watclish-icon.png" class="star-icon">
+                            <h5>${data.Runtime}</h5>
+                            <h5 class="genre">${data.Genre}</h5>
+                                <img src="./images/my-watclish-icon.png" class="add-watchlist-icon">
                             <h5 class="add-watchlist">Watchlist</h5>
                         
                         </div>
 
 
                         <div class="movie-info">
-                            <p class="movie-text">${movie.Plot}</p>
+                            <p class="movie-text">${data.Plot}</p>
                         </div>
             
                     </div>
@@ -59,7 +59,7 @@ function renderMovies() {
 
           
             `;
-          }
+          
         }
       });
 
