c492ab4cb1ea8da64f76acfd5e995997e97e8fdf
Fulya <fulya.ertay@gmail.com>
2023-03-06 17:52:55 +0300

    added renderMovies

--- 5-movie-watchlist/index.js
+++ 5-movie-watchlist/index.js
@@ -1,1 +1,19 @@
 "https://www.omdbapi.com/?t=aa&apikey=60c3cf12"
+
+const submitBtn = document.querySelector(".submit-btn")
+const searchInput = document.querySelector(".search-input")
+let container = document.querySelector(".container")
+
+function renderMovies(){
+    if (searchInput.value==""){
+        container.innerHTML=`
+        <img src="./images/no-data-initial.png" class="no-data" />
+        `
+
+    }
+}
+
+
+
+
+renderMovies()
