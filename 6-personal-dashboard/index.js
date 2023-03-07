96d3dccb2d523dbfc45d786ee9ed516cf0072b1a
Fulya <fulya.ertay@gmail.com>
2023-03-07 13:29:18 +0300

    get image from unsplash

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -1,58 +1,109 @@
+/**
+ * Challenge: get a random image from Unsplash and set it as the background
+ * 
+ * Part 1:
+ * 
+ * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
+ * (You can change the "query" at the end to whatever theme you want)
+ * 
+ * Change the body's backgroundImage to: 
+ * `url(<insert the URL of the iamge from the API here>)`
+ * 
+ * (You may need to dig around the response body a bit to find this URL)
+ * 
+ * (Note I've already added some CSS to resize the image within the window.
+ * Instructions for this were found on CSS Tricks: 
+ * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
+ */
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.body.style.backgroundImage = `url(${data.urls.full})`
     })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
-
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
-
-function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+    
+/**
+ * {
+	id: "KMn4VEeEPR8",
+	created_at: "2017-10-09T01:04:47-04:00",
+	updated_at: "2021-06-02T09:02:25-04:00",
+	promoted_at: "2017-10-09T09:31:35-04:00",
+	width: 4621,
+	height: 3072,
+	color: "#c0c0c0",
+	blur_hash: "LXL|_q}qn$IoPAn%Rjj?ogNboLs:",
+	description: "The last night of a two week stay on the North Shore of Oahu, Hawaii.",
+	alt_description: "seashore during golden hour",
+	urls: {
+		raw: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1",
+		full: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=85",
+		regular: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=80&w=1080",
+		small: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=80&w=400",
+		thumb: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=80&w=200"
+	},
+	links: {
+		self: "https://api.unsplash.com/photos/KMn4VEeEPR8",
+		html: "https://unsplash.com/photos/KMn4VEeEPR8",
+		download: "https://unsplash.com/photos/KMn4VEeEPR8/download",
+		download_location: "https://api.unsplash.com/photos/KMn4VEeEPR8/download?ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg"
+	},
+	categories: [],
+	likes: 8437,
+	liked_by_user: false,
+	current_user_collections: [],
+	sponsorship: null,
+	user: {
+		id: "qlQ_KKvUq7k",
+		updated_at: "2021-06-02T15:34:44-04:00",
+		username: "seantookthese",
+		name: "Sean O.",
+		first_name: "Sean",
+		last_name: "O.",
+		twitter_username: "notseano",
+		portfolio_url: "http://www.seanoulashin.com",
+		bio: "22-year-old magician and lifestyle photographer living in Portland, Oregon.",
+		location: "Portland, OR",
+		links: {
+			self: "https://api.unsplash.com/users/seantookthese",
+			html: "https://unsplash.com/@seantookthese",
+			photos: "https://api.unsplash.com/users/seantookthese/photos",
+			likes: "https://api.unsplash.com/users/seantookthese/likes",
+			portfolio: "https://api.unsplash.com/users/seantookthese/portfolio",
+			following: "https://api.unsplash.com/users/seantookthese/following",
+			followers: "https://api.unsplash.com/users/seantookthese/followers"
+		},
+		profile_image: {
+			small: "https://images.unsplash.com/profile-1581798079888-298c246545d3image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+			medium: "https://images.unsplash.com/profile-1581798079888-298c246545d3image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+			large: "https://images.unsplash.com/profile-1581798079888-298c246545d3image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+		},
+		instagram_username: "notseano",
+		total_collections: 0,
+		total_likes: 6,
+		total_photos: 8,
+		accepted_tos: true,
+		for_hire: false
+	},
+	exif: {
+		make: "Canon",
+		model: "Canon EOS REBEL T3i",
+		exposure_time: "1/250",
+		aperture: "4.5",
+		focal_length: "10.0",
+		iso: 200
+	},
+	location: {
+		title: "North Shore, Waialua, United States",
+		name: "North Shore, Waialua, United States",
+		city: "Waialua",
+		country: "United States",
+		position: {
+			latitude: 21.5616575,
+			longitude: -158.0715983
+		}
+	},
+	views: 116340693,
+	downloads: 1237140
 }
-
-setInterval(getCurrentTime, 1000)
-
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `
-        })
-        .catch(err => console.error(err))
-});
+/index.html
+ */
