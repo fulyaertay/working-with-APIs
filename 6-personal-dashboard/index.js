68c7568c28c813c8e2ccaab948cf49e90127faf6
Fulya <fulya.ertay@gmail.com>
2023-03-07 14:33:28 +0300

    added crypto- get prices

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -19,15 +19,25 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         return res.json()
     })
     .then(data => {
-        /**
-         * Challenge: Add the name and icon of the cryptocurrency
-         * to the upper-left of the dashboard page
-         * 
-         * Use `data.name` and `data.image.small` to access that info
-         */
         document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
+        `
+        /**
+         * Challenge: Add the following data points underneath the 
+         * name and icon (1 paragraph each):
+         * 
+         * 1. Current price (data.market_data.current_price.usd)
+         * 2. 24-hour high price (data.market_data.high_24h.usd)
+         * 3. 24-hour low price (data.market_data.low_24h.usd)
+         * 
+         * Feel free to check the response data object for your own currency
+         * if you don't want to use USD.
+         */
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
         `
     })
     .catch(err => console.error(err))
