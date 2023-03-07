6c9ecd19b041aeb5790280d6f5e71f14522633d8
Fulya <fulya.ertay@gmail.com>
2023-03-07 16:28:16 +0300

    edited temp unit

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -39,7 +39,7 @@ function getCurrentTime() {
 setInterval(getCurrentTime, 1000)
 
 navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `)
         .then(res => {
             if (!res.ok) {
                 throw Error("Weather data not available")
