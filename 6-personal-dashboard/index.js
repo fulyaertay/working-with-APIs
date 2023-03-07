e33c66c767c40a9a85e12cd29b45bf26f2f87c06
Fulya <fulya.ertay@gmail.com>
2023-03-07 13:33:03 +0300

    add authır info

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -1,25 +1,18 @@
 /**
- * Challenge: get a random image from Unsplash and set it as the background
+ * Challenge part 2: Display the image's author
  * 
- * Part 1:
+ * With the data you've already fetched, display the name of the 
+ * image author on the page. They show up as the "user" in the data
+ * returned from the API.
  * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- * 
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
+ * Don't worry about positioning the author in the lower-left yet.
  */
 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     
 /**
