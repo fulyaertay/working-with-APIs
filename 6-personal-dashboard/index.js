da6ee6cd9e3b0b694317aa2b4b5c5ae80be102ad
Fulya <fulya.ertay@gmail.com>
2023-03-08 19:16:07 +0300

    added status of day

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -52,34 +52,29 @@ function getCurrentTime() {
     "en-us",
     { timeStyle: "short" }
   );
-  if(date.timeStyle=="AM"){
-    document.getElementById("status-day").textContent="Have a great day!"
-  }else{
-    document.getElementById("status-day").textContent="Good Evening!"
+  if (date.timeStyle == "AM") {
+    document.getElementById("status-day").textContent = "Have a great day!";
+  } else {
+    document.getElementById("status-day").textContent = "Good Evening!";
   }
 }
 
 navigator.geolocation.getCurrentPosition(async (position) => {
-    try {
-        const res = await fetch(
-            `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
-          );
-          const data = await res.json();
-          const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-          document.getElementById("weather").innerHTML = `
+  try {
+    const res = await fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
+    );
+    const data = await res.json();
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
                 <p class="weather-city">${data.name}</p>
             `;
-       
-        
-        
-    } catch (err) {
-        console.error(err);
-        
-    }
+  } catch (err) {
+    console.error(err);
+  }
 });
-
 
 //fetching from APIs
 callImage();
