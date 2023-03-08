acde9a538184fffe3c23a032a755e3d0189a8b33
Fulya <fulya.ertay@gmail.com>
2023-03-08 17:54:43 +0300

    try/catch blocks added on geolocation

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -55,17 +55,26 @@ function getCurrentTime() {
 }
 
 navigator.geolocation.getCurrentPosition(async (position) => {
-  const res = await fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
-  );
-  const data = await res.json();
-  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  document.getElementById("weather").innerHTML = `
-        <img src=${iconUrl} />
-        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-        <p class="weather-city">${data.name}</p>
-    `;
+    try {
+        const res = await fetch(
+            `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
+          );
+          const data = await res.json();
+          const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+          document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `;
+       
+        
+        
+    } catch (err) {
+        console.error(err);
+        
+    }
 });
+
 
 //fetching from APIs
 callImage();
