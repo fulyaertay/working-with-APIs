69bcb3991a856197e20ed8ec8e59d07e89bce228
Fulya <fulya.ertay@gmail.com>
2023-03-08 17:43:52 +0300

    assigned getWeatherInfo to function

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -55,21 +55,24 @@ function getCurrentTime() {
   );
 }
 
-setInterval(getCurrentTime, 1000);
-
-navigator.geolocation.getCurrentPosition(async (position) => {
-  const res = await fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
-  );
-  const data = await res.json();
-  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  document.getElementById("weather").innerHTML = `
+const getWeatherInfo = navigator.geolocation.getCurrentPosition(
+  async (position) => {
+    const res = await fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
+    );
+    const data = await res.json();
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.getElementById("weather").innerHTML = `
         <img src=${iconUrl} />
         <p class="weather-temp">${Math.round(data.main.temp)}º</p>
         <p class="weather-city">${data.name}</p>
     `;
-});
+  }
+);
 
+//fetching from APIs
 callImage();
 callDogeCoin();
 getTodo();
+setInterval(getCurrentTime, 1000);
+getWeatherInfo();
