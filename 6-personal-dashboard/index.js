4bd06796b66cf968e7dad0ac9e218b57e615fd44
Fulya <fulya.ertay@gmail.com>
2023-03-08 17:45:35 +0300

    fixed rejection issue

--- 6-personal-dashboard/index.js
+++ 6-personal-dashboard/index.js
@@ -7,13 +7,12 @@ const callImage = async () => {
 
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
-
-    // Use a default background image/author
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-    )`;
-    document.getElementById("author").textContent = `By: Dodi Achmad`;
   } catch (err) {
     console.error(err);
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+        )`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
   }
 };
 
@@ -55,24 +54,21 @@ function getCurrentTime() {
   );
 }
 
-const getWeatherInfo = navigator.geolocation.getCurrentPosition(
-  async (position) => {
-    const res = await fetch(
-      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
-    );
-    const data = await res.json();
-    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    document.getElementById("weather").innerHTML = `
+navigator.geolocation.getCurrentPosition(async (position) => {
+  const res = await fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
+  );
+  const data = await res.json();
+  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  document.getElementById("weather").innerHTML = `
         <img src=${iconUrl} />
         <p class="weather-temp">${Math.round(data.main.temp)}º</p>
         <p class="weather-city">${data.name}</p>
     `;
-  }
-);
+});
 
 //fetching from APIs
 callImage();
 callDogeCoin();
 getTodo();
 setInterval(getCurrentTime, 1000);
-getWeatherInfo();
