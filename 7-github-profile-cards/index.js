0f1e0874824b2b86b28b6c49388b9c65623f9782
Fulya <fulya.ertay@gmail.com>
2023-03-09 19:31:39 +0300

    added random users

--- 7-github-profile-cards/index.js
+++ 7-github-profile-cards/index.js
@@ -35,26 +35,29 @@ async function searchProfile(){
 }
 
 async function getProfile(){
-    const res= await fetch("https://api.github.com/users/1")
-    const data = await res.json()
-        profileCard.innerHTML+=`
-            <img src="${data.avatar_url}" class="github-avatar"/>
-            <div class="user-content">
-            <h2>${data.name}</h2>
-            <h4>${data.location}</h4>
-            <div class="statistics">
-            <h5>${data.followers} Followers</h5>
-            <h5>${data.following} Following</h5>
-            <h5>${data.public_repos} Repos</h5>
+   let users= Math.floor(Math.random()*100)+1
+   const res= await fetch(`https://api.github.com/users/${users}`)
+   const data = await res.json()
+       profileCard.innerHTML+=`
+           <img src="${data.avatar_url}" class="github-avatar"/>
+           <div class="user-content">
+           <h2>${data.name}</h2>
+           <h4>${data.location}</h4>
+           <div class="statistics">
+           <h5>${data.followers} Followers</h5>
+           <h5>${data.following} Following</h5>
+           <h5>${data.public_repos} Repos</h5>
+          
            
-            
-            </div>
-            <h5><a href="${data.html_url}"> See  Profile</a></h5>
-            
-            </div>
+           </div>
+           <h5><a href="${data.html_url}"> See  Profile</a></h5>
            
-            
-        `
+           </div>
+           `
+          
+           
+ 
+        
 }
 
 
