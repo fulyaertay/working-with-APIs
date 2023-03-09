4745dfeae2b5e0f049f791acce6015c31517a212
Fulya <fulya.ertay@gmail.com>
2023-03-09 18:56:55 +0300

    added statistics style

--- 7-github-profile-cards/index.js
+++ 7-github-profile-cards/index.js
@@ -8,7 +8,16 @@ async function getProfile(){
         profileCard.innerHTML+=`
             <img src="${data.avatar_url}" class="github-avatar"/>
             <div class="user-content">
-            <h4>${data.name}</h4>
+            <h2>${data.name}</h2>
+            <h4>${data.location}</h4>
+            <div class="statistics">
+            <h5>${data.followers} Followers</h5>
+            <h5>${data.following} Following</h5>
+            <h5>${data.public_repos} Repos</h5>
+           
+            
+            </div>
+            <h5><a href="${data.html_url}"> See  Profile</a></h5>
             
             </div>
            
