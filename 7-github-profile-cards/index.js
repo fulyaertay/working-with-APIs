c29ade5282d74d4552e0a99441f1c6a512588f92
Fulya <fulya.ertay@gmail.com>
2023-03-09 18:41:02 +0300

    added github avatar

---
+++ 7-github-profile-cards/index.js
@@ -0,0 +1,15 @@
+const searchBtn= document.querySelector(".search-btn")
+const searchInput = document.querySelector(".search-input")
+let profileCard= document.querySelector(".profile-card")
+
+async function getProfile(){
+    const res= await fetch("https://api.github.com/users/1")
+    const data = await res.json()
+        profileCard.innerHTML+=`
+            <img src="${data.avatar_url}" class="github-avatar"/>
+        
+        `
+}
+
+
+getProfile()
